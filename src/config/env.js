f4cb374f7f63205799c2f64ae2031bffac111aed
Giang Le <giangle2k7pro@gmail.com>
2026-03-12 00:31:58 +0700

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 module.exports = {
   dbConfig: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    options: {
-      encrypt: false,              // true nếu dùng Azure
-      trustServerCertificate: true // local SQL Server
-    }
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  options: {
+    instanceName: process.env.DB_INSTANCE,
+    encrypt: false,
+    trustServerCertificate: true
   }
+}
 };
