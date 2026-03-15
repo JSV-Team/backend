a086a2f84bb74e11191528845cad5ec9ab59557a
haison1234 <sonnguyen556699@gmail.com>
2026-03-15 15:57:00 +0700

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -4,9 +4,9 @@ module.exports = {
   dbConfig: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,      
+    server: process.env.DB_SERVER,
     port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,  
+    database: process.env.DB_NAME,
     options: {
       encrypt: false,
       trustServerCertificate: true
